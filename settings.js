1dce7ea520cb25c579693bfdbe66bc3fa034bc3f
Ahm356 <ahmtricks356@gmail.com>
2024-10-13 12:28:38 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,13 +3,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "923439827978"
+global.ownername = "🐛 Mr Ahmad "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
 
-global.ownernumber = '923347748038'  //creator number
+global.ownernumber = '923439827978'  //creator number
 global.ownername = ' MR WASI ' //owner name
 global.botname = 'WASI TECH BUG BOT ' //name of the bot
 
